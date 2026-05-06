559b53af6051583e5fc4e956584cc6dba0b121e1
javier Garzon <javi.garzon@hotmail.com>
2026-05-06 09:22:46 +0000

    cambios

--- lexagent_py/frontend-java/src/main/java/com/lexagent/web/LexAgentApplication.java
+++ lexagent_py/frontend-java/src/main/java/com/lexagent/web/LexAgentApplication.java
@@ -1,9 +1,7 @@
 package com.lexagent.web;
 
-import com.lexagent.web.security.AuthFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,13 +14,5 @@ public class LexAgentApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public FilterRegistrationBean<AuthFilter> authFilter(AuthFilter f) {
-        FilterRegistrationBean<AuthFilter> r = new FilterRegistrationBean<>(f);
-        r.addUrlPatterns("/*");
-        r.setOrder(1);
-        return r;
     }
 }
--- lexagent_py/frontend-java/src/main/java/com/lexagent/web/controllers/AuthController.java
+++ lexagent_py/frontend-java/src/main/java/com/lexagent/web/controllers/AuthController.java
@@ -1,14 +1,18 @@
 package com.lexagent.web.controllers;
 
-import com.lexagent.web.client.LexAgentApiClient;
-import jakarta.servlet.http.HttpSession;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Map;
+import com.lexagent.web.client.LexAgentApiClient;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AuthController {
